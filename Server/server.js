1d05d34471cd99b2a4c58863cc67f71e3ae6d03f
Sahil <garg07825@gmail.com>
2024-01-11 15:11:02 +0530

    basic setup is ready for backend

---
+++ Server/server.js
@@ -0,0 +1,8 @@
+import connectDb from "./config/connectDb.js";
+import { app } from "./index.js";
+const port = process.env.PORT;
+
+// here first i am connecting with database and then i am starting the server
+connectDb().then(
+  app.listen(port, () => console.log(`Server is Working on ${port}`))
+);
